bec386949c09c273e019dad5c749a5f0189de808
GiacomoSaccaggi <44114264+GiacomoSaccaggi@users.noreply.github.com>
2019-03-28 02:29:21 +0100

    Add files via upload

---
+++ inail/secondo_simulatore/js/auth.js
@@ -0,0 +1,6 @@
+var app = angular.module('auth', []);
+app.controller('authctrl', function($scope) {
+    $scope.username1 = "fastclick18";
+    $scope.password1 = "0618";
+    $scope.codazienda = "+5g3422739c53casced46afce754ad713d34da03f19ead5321afa19afdbf7c996";
+});
