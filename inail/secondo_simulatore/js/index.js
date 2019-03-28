113d8884ce6e5023cdba1a401c0d2e74e780d9da
Giacomo Saccaggi <44114264+GiacomoSaccaggi@users.noreply.github.com>
2019-03-28 02:29:21 +0100

    Add files via upload

---
+++ inail/secondo_simulatore/js/index.js
@@ -0,0 +1,8 @@
+//copy the codice azienda to the clipboard
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
