0c909a4f5affded550c51de2b3562d9c02d7b7d0
giacomo.saccaggi@gmail.com <giacomo.saccaggi@gmail.com>
2019-08-08 13:44:30 +0200

    update

--- inail/js/index.js
+++
@@ -1,8 +0,0 @@
-//copy the codice azienda to the clipboard
-function copyToClipboard(element) {
-    var $temp = $("<input>");
-    $("body").append($temp);
-    $temp.val($(element).text()).select();
-    document.execCommand("copy");
-    $temp.remove();
-}
